6bbdfe25907714880295765d0813c6cf1f1d6eb8
Stef Havermans <stef.havermans@gmail.com>
2020-10-09 10:40:52 +0200

    Update package-lock.json, package.json, and 3 more files...

--- src/App.js
+++ src/App.js
@@ -1,61 +1,72 @@
-import React, { Component } from "react";
+import React from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styled from 'styled-components';
 
 import Graph from "./Graph";
 
-const randomData = () =>
-  Array.from({ length: 10 }, () => Math.floor(Math.random() * 100));
-
-class App extends Component {
-  state = {
-    datasets: [
-      {
-        color: "light-blue",
-        values: randomData()
-      }
-    ]
-  };
-  randomize = () => {
-    this.setState({
-      datasets: [
-        {
-          values: randomData()
-        }
-      ]
-    });
-  };
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React wrapper for frappe-charts</h1>
-        </header>
+const App = () => (
+      <Root>
+        <Header>
+          <Logo {...{ src: logo, alt: 'logo '}} />
+          <Title>React wrapper for frappe-charts</Title>
+        </Header>
         <Graph
           title="Bar Chart"
           type="bar"
           data={{
-            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-            datasets: this.state.datasets
+            labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
+            datasets: [{
+              name: "Some Data", chartType: 'bar',
+              values: [25, 40, 30, 35, 8, 52, 17, -4]
+            },
+            {
+              name: "Another Set", chartType: 'bar',
+              values: [25, 50, -10, 15, 18, 32, 27, 14]
+            }],
           }}
           onSelect={a => console.log(a.index)}
         />
-        <button onClick={this.randomize}>Update Data</button>
+        <button {...{ onClick: () => {} }}>Update Data</button>
         <Graph
           title="Line Chart"
           type="line"
           data={{
-            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-            datasets: this.state.datasets
+            labels: ["1", "2", "3", "4", "5", "6", "7", "8"],
+            datasets: [{
+              name: "Some Data", chartType: 'bar',
+              values: [25, 40, 30, 35, 8, 52, 17, -4]
+            },
+            {
+              name: "Another Set", chartType: 'bar',
+              values: [25, 50, -10, 15, 18, 32, 27, 14]
+            }],
           }}
           show_dots={false}
           heatline
           region_fill
         />
-      </div>
-    );
-  }
-}
+      </Root>
+);
+
+const Root = styled(App)`
+text-align: center;
+`;
+
+const Logo = styled.img`
+text-align: center;
+`;
+
+const Header = styled.header`
+background: #252830;
+color: white;
+display: flex;
+justify-content: center;
+align-items: center;
+padding: 20px;
+`;
+
+const Title = styled.h1`
+margin-left: 30px;
+`;
 
 export default App;
